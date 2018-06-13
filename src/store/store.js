02f86b657bf1809904de04dda8a8af3b0e402b04
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-06-13 00:01:52 -0700

    Persist auth state through local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,31 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import reduxThunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import filter from 'redux-localstorage-filter';
 
 import {rootReducer} from './reducers';
-import {login} from './authentication/actions';
 
-export const store = createStore(
-  rootReducer(),
-  applyMiddleware(reduxThunk, createLogger())
+const storage = compose(
+  filter(['authentication.authentication.jwt', 'authentication.authentication.isLoggedIn'])
+)(adapter(
+  window.localStorage
+));
+
+const enhancer = compose(
+  applyMiddleware(
+    reduxThunk,
+    createLogger()
+  ),
+  persistState(storage, 'redux')
 );
 
-store.dispatch(login('jshepherd@harding.edu', 'mypassword'));
+const reducer = compose(
+  mergePersistedState()
+)(rootReducer());
+
+export const store = createStore(
+  reducer,
+  enhancer
+);

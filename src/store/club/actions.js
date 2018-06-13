cf093e3969889d9eb96214c0752b7368f430f608
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-06-13 00:01:32 -0700

    Get JWT from state rather than method argument

--- src/store/club/actions.js
+++ src/store/club/actions.js
@@ -6,21 +6,19 @@ export const FETCH_CLUBS_ERROR = 'FETCH_CLUBS_ERROR';
 
 export const INVALIDATE_CLUBS = 'INVALIDATE_CLUBS';
 
-export function fetchClubs (jwt) {
-  return function (dispatch) {
+export function fetchClubs () {
+  return function (dispatch, getState) {
     (async function () {
+      dispatch(fetchClubsBegin());
       try {
-        dispatch(fetchClubsBegin());
         let res = await fetch('http://localhost:8080/api/club', {
           credentials: 'include',
           headers: {
-            'Authorization': jwt
+            'Authorization': getState().authentication.authentication.jwt
           }
         });
         let json = await res.json();
         if (!res.ok) {
-          console.log(res);
-          console.log(json);
           dispatch(fetchClubsError({
             title: res.status + ' ' + res.statusText,
             message: json.error.name + ': ' + json.error.message

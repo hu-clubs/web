69c4242a467d9ac556d0c80c9263855c803e4bde
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-06-12 23:59:42 -0700

    Add Error page, conditionally render retry button in ErrorNotification

--- src/components/error/ErrorNotification.js
+++ src/components/error/ErrorNotification.js
@@ -1,23 +1,23 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './Error.css';
+import './ErrorNotification.css';
 
-class Error extends Component {
+class ErrorNotification extends Component {
   render () {
     return (
       <div className='error notification is-danger'>
         {this.props.title && <h2 className='error--title'>{this.props.title}</h2>}
-        {this.props.message && (<p className='error--message'>{this.props.message}</p>)}<br />
-        {this.props.retry || <button className='button is-danger is-inverted is-fullwidth'>Retry</button>}
+        {this.props.message && <p className='error--message'>{this.props.message}</p>}
+        {this.props.retry && (<button className='button is-danger is-inverted is-fullwidth'>Retry</button>)}
       </div>
     );
   }
 }
 
-Error.propTypes = {
+ErrorNotification.propTypes = {
   title: PropTypes.string,
   message: PropTypes.string,
   retry: PropTypes.bool
 };
 
-export default Error;
+export default ErrorNotification;

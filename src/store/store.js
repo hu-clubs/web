717c6f14ab5bc6755fe449451e66cc732f25f17e
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-08-20 13:36:20 -0500

    Fix npm audit. Add jwt-decode to dependencies

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,10 @@ import filter from 'redux-localstorage-filter';
 import adapter from 'redux-localstorage/lib/adapters/localStorage';
 import {createLogger} from 'redux-logger';
 import reduxThunk from 'redux-thunk';
-
 import {rootReducer} from './reducers';
 
+// This allows redux state to be persisted in local storage
+// Right now this is used to store the authentication token between visits
 const storage = compose(
   filter(['authentication.jwt'])
 )(adapter(
